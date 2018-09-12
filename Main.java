1368379e62fe53c592731b40f6738fd70d9f9a48
wangjason2020 <42677901+wangjason2020@users.noreply.github.com>
2018-09-12 09:11:47 -0700

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,7 @@
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        System.out.println("l(a\n\nle\naf\nfa\n\nll\n\ns)\none\nl\n\niness");
+    }
+}
